c555c88d3c2aa1a2b51cade4c618cea54b0a4f86
Samuel Riddle <30331129+sjriddle@users.noreply.github.com>
2018-12-30 14:11:28 -0700

    Update SpiderForm.Designer.cs

--- WebCrawler/Assign7/SpiderForm.Designer.cs
+++ WebCrawler/Assign7/SpiderForm.Designer.cs
@@ -48,9 +48,8 @@
             this.msSpiderForm.SuspendLayout();
             this.tsSpiderForm.SuspendLayout();
             this.SuspendLayout();
-            // 
+            
             // msSpiderForm
-            // 
             this.msSpiderForm.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiFile,
             this.tsmiSpider,
@@ -60,24 +59,21 @@
             this.msSpiderForm.Size = new System.Drawing.Size(565, 24);
             this.msSpiderForm.TabIndex = 0;
             this.msSpiderForm.Text = "menuStrip1";
-            // 
+            
             // tsmiFile
-            // 
             this.tsmiFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiExit});
             this.tsmiFile.Name = "tsmiFile";
             this.tsmiFile.Size = new System.Drawing.Size(37, 20);
             this.tsmiFile.Text = "File";
-            // 
+            
             // tsmiExit
-            // 
             this.tsmiExit.Name = "tsmiExit";
             this.tsmiExit.Size = new System.Drawing.Size(152, 22);
             this.tsmiExit.Text = "Exit";
             this.tsmiExit.Click += new System.EventHandler(this.ExitMenuItem_Click);
-            // 
+            
             // tsmiSpider
-            // 
             this.tsmiSpider.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiNewSpider,
             this.tsmiAbortSpider,
@@ -85,47 +81,41 @@
             this.tsmiSpider.Name = "tsmiSpider";
             this.tsmiSpider.Size = new System.Drawing.Size(52, 20);
             this.tsmiSpider.Text = "Spider";
-            // 
+            
             // tsmiNewSpider
-            // 
             this.tsmiNewSpider.Name = "tsmiNewSpider";
             this.tsmiNewSpider.Size = new System.Drawing.Size(152, 22);
             this.tsmiNewSpider.Text = "New";
             this.tsmiNewSpider.Click += new System.EventHandler(this.NewSpiderMenuItem_Click);
-            // 
+ 
             // tsmiAbortSpider
-            // 
             this.tsmiAbortSpider.Enabled = false;
             this.tsmiAbortSpider.Name = "tsmiAbortSpider";
             this.tsmiAbortSpider.Size = new System.Drawing.Size(152, 22);
             this.tsmiAbortSpider.Text = "Abort";
             this.tsmiAbortSpider.Click += new System.EventHandler(this.AbortMenuItem_Click);
-            // 
+            
             // tsmiRemoveSpider
-            // 
             this.tsmiRemoveSpider.Enabled = false;
             this.tsmiRemoveSpider.Name = "tsmiRemoveSpider";
             this.tsmiRemoveSpider.Size = new System.Drawing.Size(152, 22);
             this.tsmiRemoveSpider.Text = "Clean away";
             this.tsmiRemoveSpider.Click += new System.EventHandler(this.RemoveMenuItem_Click);
-            // 
+            
             // tsmiHelp
-            // 
             this.tsmiHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmiAbout});
             this.tsmiHelp.Name = "tsmiHelp";
             this.tsmiHelp.Size = new System.Drawing.Size(44, 20);
             this.tsmiHelp.Text = "Help";
-            // 
+            
             // tsmiAbout
-            // 
             this.tsmiAbout.Name = "tsmiAbout";
             this.tsmiAbout.Size = new System.Drawing.Size(152, 22);
             this.tsmiAbout.Text = "About";
             this.tsmiAbout.Click += new System.EventHandler(this.AboutMenuItem_Click);
-            // 
+ 
             // tsSpiderForm
-            // 
             this.tsSpiderForm.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tslSpiderForm,
             this.tscbSpiderForm,
@@ -137,23 +127,20 @@
             this.tsSpiderForm.Size = new System.Drawing.Size(565, 25);
             this.tsSpiderForm.TabIndex = 1;
             this.tsSpiderForm.Text = "toolStrip1";
-            // 
+            
             // tslSpiderForm
-            // 
             this.tslSpiderForm.Font = new System.Drawing.Font("Miramonte", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tslSpiderForm.Name = "tslSpiderForm";
             this.tslSpiderForm.Size = new System.Drawing.Size(46, 22);
             this.tslSpiderForm.Text = "Spider:";
-            // 
+ 
             // tscbSpiderForm
-            // 
             this.tscbSpiderForm.DropDownWidth = 260;
             this.tscbSpiderForm.Name = "tscbSpiderForm";
             this.tscbSpiderForm.Size = new System.Drawing.Size(300, 25);
             this.tscbSpiderForm.SelectedIndexChanged += new System.EventHandler(this.cbSpiders_SelectedIndexChanged);
-            // 
+            
             // tsbNewSpider
-            // 
             this.tsbNewSpider.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.tsbNewSpider.Image = global::Assign7.Properties.Resources.spider_new;
             this.tsbNewSpider.ImageTransparentColor = System.Drawing.Color.Magenta;
@@ -162,9 +149,8 @@
             this.tsbNewSpider.Text = "toolStripButton1";
             this.tsbNewSpider.ToolTipText = "New Spider";
             this.tsbNewSpider.Click += new System.EventHandler(this.NewSpiderMenuButton_Click);
-            // 
+ 
             // tsbAbortSpider
-            // 
             this.tsbAbortSpider.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.tsbAbortSpider.Enabled = false;
             this.tsbAbortSpider.Image = global::Assign7.Properties.Resources.spider_abort;
@@ -174,9 +160,8 @@
             this.tsbAbortSpider.Text = "toolStripButton2";
             this.tsbAbortSpider.ToolTipText = "Cancel Spider";
             this.tsbAbortSpider.Click += new System.EventHandler(this.AbortMenuButton_Click);
-            // 
+ 
             // tsbRemoveSpider
-            // 
             this.tsbRemoveSpider.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.tsbRemoveSpider.Enabled = false;
             this.tsbRemoveSpider.Image = global::Assign7.Properties.Resources.spider_clean;
@@ -186,9 +171,8 @@
             this.tsbRemoveSpider.Text = "toolStripButton3";
             this.tsbRemoveSpider.ToolTipText = "Remove Spider";
             this.tsbRemoveSpider.Click += new System.EventHandler(this.RemoveMenuButton_Click);
-            // 
-            // lbSpiderStatus
-            // 
+
+            // lbSpiderStatus 
             this.lbSpiderStatus.BackColor = System.Drawing.SystemColors.ControlLight;
             this.lbSpiderStatus.Font = new System.Drawing.Font("Miramonte", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbSpiderStatus.Location = new System.Drawing.Point(4, 53);
@@ -196,9 +180,8 @@
             this.lbSpiderStatus.Size = new System.Drawing.Size(561, 28);
             this.lbSpiderStatus.TabIndex = 2;
             this.lbSpiderStatus.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
+            
             // tcMessages
-            // 
             this.tcMessages.Appearance = System.Windows.Forms.TabAppearance.Buttons;
             this.tcMessages.ItemSize = new System.Drawing.Size(0, 1);
             this.tcMessages.Location = new System.Drawing.Point(4, 75);
@@ -207,9 +190,8 @@
             this.tcMessages.Size = new System.Drawing.Size(561, 335);
             this.tcMessages.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
             this.tcMessages.TabIndex = 3;
-            // 
+            
             // SpiderForm
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 414);
@@ -226,9 +208,7 @@
             this.tsSpiderForm.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
-
         #endregion
 
         private System.Windows.Forms.MenuStrip msSpiderForm;
